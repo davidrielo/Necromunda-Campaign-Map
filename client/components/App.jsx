8286e1b554750c93c3a1cde5b51eabd022937b05
Ross Howard <ross_howard@dennis.co.uk>
2019-02-07 16:50:25 +0000

    002 - Attempting to import JSON

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,42 @@
 /*
     ./client/components/App.jsx
 */
-import React from 'react';
+import React, { Component } from 'react';
+class App extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      data:[],
+    }
+  }
 
-export default class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Twatticus</h1>
-      </div>);
+  componentDidMount()
+  {
+    fetch('./gangs/danB.json').
+    then((Response)=>Response.json()).
+    then((findresponse)=>
+    {
+      this.setState({
+        data:findresponse.movies
+      })
+    })
+  }
+  render()
+  {
+    return(
+      <div>
+        {
+          this.state.data.map((dynamicData,key)=>
+          <div>
+            <span>{dynamicData.gang}</span>
+            {dynamicData.gang}
+          </div>
+          )
+        }
+      </div>
+    )
   }
 }
+
+export default App;
